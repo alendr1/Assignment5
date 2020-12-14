88001dd312d8ed51215c1f8d1771b6a5d85334db
alendr1 <at1604@nyu.edu>
2020-12-13 23:19:02 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -1,160 +1,50 @@
-from socket import *
-import socket
-import os
-import sys
-import struct
-import time
-import select
-import binascii
+### welcome_assignment_answers
+### Input - All eight questions given in the assignment.
+### Output - The right answer for the specific question.
 
-ICMP_ECHO_REQUEST = 8
-MAX_HOPS = 30
-TIMEOUT = 2.0
-TRIES = 1
-
-
-# The packet that we shall send to each router along the path is the ICMP echo
-# request packet, which is exactly what we had used in the ICMP ping exercise.
-# We shall use the same packet that we built in the Ping exercise
-
-def checksum(str_):
-    # In this function we make the checksum of our packet
-    str_ = bytearray(str_)
-    csum = 0
-    countTo = (len(str_) // 2) * 2
-
-    for count in range(0, countTo, 2):
-        thisVal = str_[count + 1] * 256 + str_[count]
-        csum = csum + thisVal
-        csum = csum & 0xffffffff
-
-    if countTo < len(str_):
-        csum = csum + str_[-1]
-        csum = csum & 0xffffffff
-
-    csum = (csum >> 16) + (csum & 0xffff)
-    csum = csum + (csum >> 16)
-    answer = ~csum
-    answer = answer & 0xffff
-    answer = answer >> 8 | (answer << 8 & 0xff00)
-    return answer
+def welcome_assignment_answers(question):
+    #The student doesn't have to follow the skeleton for this assignment.
+    #Another way to implement is using a "case" statements similar to C.
+    if question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    elif question ==  "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question ==  "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
+        answer = "42b76fe51778764973077a5a94056724"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+        answer = 5
+    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
+        answer = 4
+    return(answer)
+# Complete all the questions.
 
 
-def build_packet():
-    # In the sendOnePing() method of the ICMP Ping exercise ,firstly the header of our
-    # packet to be sent was made, secondly the checksum was appended to the header and
-    # then finally the complete packet was sent to the destination.
-
-    # Make the header in a similar way to the ping exercise.
-    myChecksum = 0
-    myID = os.getpid() & 0xFFFF
-
-    # Make a dummy header with a 0 checksum.
-    # struct -- Interpret strings as packed binary data
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myID, 1)
-    # header = struct.pack("!HHHHH", ICMP_ECHO_REQUEST, 0, myChecksum, pid, 1)
-    data = struct.pack("d", time.time())
-
-    # Calculate the checksum on the data and the dummy header.
-    # Append checksum to the header.
-    myChecksum = checksum(header + data)
-    if sys.platform == 'darwin':
-        myChecksum = socket.htons(myChecksum) & 0xffff
-        # Convert 16-bit integers from host to network byte order.
-    else:
-        myChecksum = htons(myChecksum)
-
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myID, 1)
-    packet = header + data
-    return packet
-
-
-def ip_to_host(addr):
-    """ convert an IP address to a host name, returning shortname and fqdn to the
-        caller
-    """
-
-    try:
-        fqdn = socket.gethostbyaddr(addr)[0]
-        shortname = fqdn.split('.')[0]
-        if fqdn == shortname:
-            fqdn = ""
-
-    except:
-        # can't resolve it, so default to the address given
-        shortname = addr
-        fqdn = "hostname not returnable"
-
-    return fqdn
-
-
-def get_route(hostname):
-    timeLeft = TIMEOUT
-    # This is your list to use when iterating through each trace
-    tracelist2 = []  # This is your list to contain all traces
-
-    for ttl in range(1, MAX_HOPS):
-        for tries in range(TRIES):
-            destAddr = socket.gethostbyname(hostname)
-            tracelist1 = []
-            tracelist2.append(tracelist1)
-            # Fill in start
-            # Make a raw socket named mySocket
-            icmp = socket.getprotobyname("icmp")
-            mySocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
-            # mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, icmp)
-            # Fill in end
-
-            mySocket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, struct.pack('I', ttl))
-            mySocket.settimeout(TIMEOUT)
-            try:
-                d = build_packet()
-                mySocket.sendto(d, (hostname, 0))
-                t = time.time()
-                startedSelect = time.time()
-                whatReady = select.select([mySocket], [], [], timeLeft)
-                howLongInSelect = (time.time() - startedSelect)
-
-                if whatReady[0] == []:  # Timeout
-                    tracelist1.append("*    *    * Request timed out.")
-                tracelist1.append(whatReady[0])
-                recvPacket, addr = mySocket.recvfrom(1024)
-                timeReceived = time.time()
-                timeLeft = timeLeft - howLongInSelect
-
-                if timeLeft <= 0:
-                    tracelist1.append("*    *    * Request timed out.")
-
-            except socket.timeout:
-                continue
-
-            else:
-                icmpHeader = recvPacket[20:28]
-                request_type, code, checksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
-                print(request_type)
-                if request_type == 11:
-                    bytes = struct.calcsize("d")
-                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                    tracelist1.append(
-                        (str(ttl), str(round((timeReceived - t) * 1000)) + "ms", addr[0], ip_to_host(addr[0])))
-
-                elif request_type == 3:
-                    bytes = struct.calcsize("d")
-                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                    tracelist1.append(
-                        (str(ttl), str(round((timeReceived - t) * 1000)) + "ms", addr[0], ip_to_host(addr[0])))
-
-
-                elif request_type == 0:
-                    bytes = struct.calcsize("d")
-                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                    tracelist1.append(
-                        (str(ttl), str(round((timeReceived - t) * 1000)) + "ms", addr[0], ip_to_host(addr[0])))
-                    print(tracelist2)
-                    return tracelist2
-                else:
-                    tracelist1.append("error")
-                    break
-            finally:
-
-                mySocket.close()
+if __name__ == "__main__":
+    #use this space to debug and verify that the program works
+    debug_question = "Are encoding and encryption the same? - Yes/No"
+    print(debug_question)
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
+    print(debug_question)
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "Is a hashed message supposed to be un-hashed? - Yes/No"
+    print(debug_question)
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code"
+    print(debug_question)
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "Is MD5 a secured hashing algorithm? - Yes/No"
+    print(debug_question)
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
+    print(debug_question)
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
+    print(debug_question)
+    print(welcome_assignment_answers(debug_question))
